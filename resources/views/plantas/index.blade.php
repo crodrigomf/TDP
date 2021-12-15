ed1b8ee79564d221bdc726dc61887b3bf072865e
Carlos Rodrigo <crodrigomf@gmail.com>
2021-12-15 10:02:16 -0300

    GH-6 Template

--- resources/views/plantas/index.blade.php
+++ resources/views/plantas/index.blade.php
@@ -1,6 +1,17 @@
+<x-app-layout>
+
 <h1>Você está em sua página de plantas</h1>
 @foreach (Auth::user()->plantas as $planta)
    <p> {{$planta->popular}} </p>
 @endforeach
 
 <a href="{{ route('plantas.create') }}">Inserir</a>
+
+<x-slot name="header">
+
+   Plantas
+
+</x-slot>
+
+
+</x-app-layout>
